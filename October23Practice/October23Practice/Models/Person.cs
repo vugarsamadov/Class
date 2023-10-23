1c0936252f76a4c362543da3f0e38953e2e3dd31
mahammadmehdi <mi779pf7t@code.edu.az>
2023-10-23 11:09:38 +0300

    A

---
+++ October23Practice/October23Practice/Models/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace October23Practice.Models
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+
+        private string _surname;
+        private DateTime _birthday;
+
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                _name = IsValidName(value) ? value : null!;
+            }
+        }
+
+        public int Age 
+        {
+            get => _age;
+            set
+            {
+                _age = IsValidAge(value) ? value : 0;
+            }
+        }
+
+        public int GetAge() => _age;
+        public string GetName() => _name;
+
+        public void SetAge(int Age)
+        {
+            _age = IsValidAge(Age) ? Age : 0;
+        }
+        public void SetName(string Name)
+        {
+            _name = IsValidName(Name) ? Name : null!;
+        }
+
+
+
+
+        private bool IsValidName(string name)
+        {
+            if(name.Length < 2)
+            {
+                Console.WriteLine("Name invalid!");
+                return false;
+            }
+            return true;
+        }
+        private bool IsValidAge(int age)
+        {
+            if(age < 16)
+            {
+                Console.WriteLine("Age is smaller than 16!");
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
