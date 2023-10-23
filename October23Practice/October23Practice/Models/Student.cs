6ab1d408f933d16911cdb9b83233ba868dc0967a
mahammadmehdi <mi779pf7t@code.edu.az>
2023-10-23 12:12:29 +0300

    A

--- October23Practice/October23Practice/Models/Student.cs
+++ October23Practice/October23Practice/Models/Student.cs
@@ -8,6 +8,14 @@ namespace October23Practice.Models
 {
     internal class Student : Person
     {
+        private bool _hasScholarship;
 
+        public bool HasScholarship  { get => _hasScholarship; set => _hasScholarship = value; }
+
+        public override string ToString()
+        {
+            string prompt = HasScholarship ? "This one is such a good student" : "This one doesn't study hard";
+            return prompt;
+        }
     }
 }
--- October23Practice/October23Practice/Program.cs
+++ October23Practice/October23Practice/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace October23Practice
+﻿using October23Practice.Models;
+
+namespace October23Practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student()
+            {
+                Name = "Vugar",
+                Age = 20,
+                HasScholarship = false,
+            };
+            
+            Student student2 = new Student()
+            {
+                Name = "Hidayat",
+                Age = 20,
+                HasScholarship = true,
+            };
+
+            Console.WriteLine(student);
+            
+            Console.WriteLine(student2);
         }
     }
 }
