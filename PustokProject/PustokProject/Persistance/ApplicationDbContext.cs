3795c383490a7635c06c6f0d9a622ef842e20f36
unknown <mi779pf7t@code.edu.az>
2023-11-30 13:39:44 +0400

    A

---
+++ PustokProject/PustokProject/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PustokProject.CoreModels;
+
+namespace PustokProject.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Slider> Sliders {  get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer(@"Server=DESKTOP-MV8SC5T\SQLEXPRESS;Database=PracticeNov30Pustok;TrustServerCertificate=True;Encrypt=False;Trusted_Connection=True");
+        }
+    }
+}
